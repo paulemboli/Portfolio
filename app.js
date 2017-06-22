7268301934566fe4fa675bd63286a2e551858225
pauleboli <paulemboli@gmail.com>
2017-06-22 16:37:39 -0400

    init

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ const app = express();
 app.use (express.static( "public"))// to have access to pictures
 app.use(parser.json());
 
-var connectionString = "postgres://postgres:1234567890@localhost:5432/portfolio";
+var connectionString = "postgres://wybkmycmhctnpn:42cdab1d0e895ea72b11bafbff840004d8c75f053608e0940b9c01eeec95a07c@ec2-23-23-111-171.compute-1.amazonaws.com:5432/d7nsq7k0pfaa3";
 var pgClient = new pg.Client(connectionString);// new instant of Client
 pgClient.connect();
 
 //Tested it out if it is connected by creating a table
-// pgClient.query("CREATE TABLE IF NOT EXISTS blog(id SERIAL UNIQUE PRIMARY KEY, title varchar(255) NOT NULL, body text NOT NULL)")
+pgClient.query("CREATE TABLE IF NOT EXISTS blog(id SERIAL UNIQUE PRIMARY KEY, title varchar(255) NOT NULL, body text NOT NULL)")
 app.set('view engine', 'ejs')
 app.get('/',(req ,res)=> {
   res.render('index');
@@ -57,4 +57,4 @@ app.post('/posts', function (req,res) {
 res.redirect(200,'/blog');
 })
 
-app.listen(8000)
+app.listen(process.env.PORT)
